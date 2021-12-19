9cfbebd6448f4dbda2b72e144a8161dab74ad717
angiealmeida <angie.almeida.mt@correo.usa.edu.co>
2021-12-18 22:58:06 -0500

    first commit

---
+++ src/main/java/Reto3_Web/servicio/UserService.java
@@ -0,0 +1,144 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reto3_Web.servicio;
+
+import Reto3_Web.modelo.User;
+import Reto3_Web.repositorio.UserRepositorio;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Angie Almeida
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepositorio userRepository;
+
+    /**
+     * Lista usuarios
+     * @return 
+     */
+    public List<User> getAll() {
+        return userRepository.getAll();
+    }
+
+    /**
+     * Optional
+     * @param id
+     * @return 
+     */
+    public Optional<User> getUser(int id) {
+
+        return userRepository.getUser(id);
+    }
+
+    /**
+     * Crear
+     * @param user
+     * @return 
+     */
+    public User create(User user) {
+        if (user.getId() == null) {
+            return user;
+        } else {
+            Optional<User> e = userRepository.getUser(user.getId());
+            if (e.isEmpty()) {
+                if (emailExists(user.getEmail()) == false) {
+                    return userRepository.create(user);
+                } else {
+                    return user;
+                }
+            } else {
+                return user;
+            }
+        }
+    }
+
+    /**
+     * Actualizar
+     * @param user
+     * @return 
+     */
+    public User update(User user) {
+
+        if (user.getId() != null) {
+            Optional<User> userDb = userRepository.getUser(user.getId());
+            if (!userDb.isEmpty()) {
+                if (user.getIdentification() != null) {
+                    userDb.get().setIdentification(user.getIdentification());
+                }
+                if (user.getName() != null) {
+                    userDb.get().setName(user.getName());
+                }
+                if (user.getAddress() != null) {
+                    userDb.get().setAddress(user.getAddress());
+                }
+                if (user.getCellPhone() != null) {
+                    userDb.get().setCellPhone(user.getCellPhone());
+                }
+                if (user.getEmail() != null) {
+                    userDb.get().setEmail(user.getEmail());
+                }
+                if (user.getPassword() != null) {
+                    userDb.get().setPassword(user.getPassword());
+                }
+                if (user.getZone() != null) {
+                    userDb.get().setZone(user.getZone());
+                }
+
+                userRepository.update(userDb.get());
+                return userDb.get();
+            } else {
+                return user;
+            }
+        } else {
+            return user;
+        }
+    }
+
+    /**
+     * Eliminar
+     * @param userId
+     * @return 
+     */
+    public boolean delete(int userId) {
+        Boolean aBoolean = getUser(userId).map(user -> {
+            userRepository.delete(user);
+            return true;
+        }).orElse(false);
+        return aBoolean;
+    }
+
+    /**
+     * Verificar email
+     * @param email
+     * @return 
+     */
+    public boolean emailExists(String email) {
+        return userRepository.emailExists(email);
+    }
+
+    /**
+     * Autentificación del usuario
+     * @param email
+     * @param password
+     * @return 
+     */
+    public User authenticateUser(String email, String password) {
+        Optional<User> usuario = userRepository.authenticateUser(email, password);
+
+        if (usuario.isEmpty()) {
+            return new User();
+        } else {
+            return usuario.get();
+        }
+    }
+
+}
